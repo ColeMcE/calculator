057e2f8378068605d1f8f47e6d44e2107c770fe6
ColeMcE <27023447+ColeMcE@users.noreply.github.com>
2023-02-22 15:31:39 -0600

    Add ability to press number keys

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
-let buttons = document.getElementsByClassName("button");
-console.log(buttons);
-for (let i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener("click", onClick);
+let numberButtons = document.getElementsByClassName("number");
+let calcInput = document.getElementById("number-display");
+
+calcInput.innerHTML = "0";
+
+for (let i = 0; i < numberButtons.length; i++) {
+    numberButtons[i].addEventListener("click", numbInput);
     
 }
 
-function onClick () {
-    console.log("click");
+function numbInput () {
+    console.log("numbah heya oh yeah wow cool lemmeseeit");
+    if (calcInput.innerHTML.length < 9)    
+        if (calcInput.innerHTML == "0") {
+            calcInput.innerHTML = this.innerHTML
+        }
+        else {
+            calcInput.innerHTML += this.innerHTML;
+        }
 }
+
+
+
