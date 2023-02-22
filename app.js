6d0fd0696f8e1c14e95261e48867181d865ee84c
ColeMcE <27023447+ColeMcE@users.noreply.github.com>
2023-02-22 16:23:04 -0600

    Add functionality for AC button

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
+//Declare Variables
 let numberButtons = document.getElementsByClassName("number");
 let calcInput = document.getElementById("number-display");
+let allClear = document.getElementById("ac-button");
 
+//Set initial display value
 calcInput.innerHTML = "0";
 
+//Add event listeners
 for (let i = 0; i < numberButtons.length; i++) {
     numberButtons[i].addEventListener("click", numbInput);
     
 }
+allClear.addEventListener("click", clearAll);
 
+//Updates number display when number is clicked
 function numbInput () {
     console.log("numbah heya oh yeah wow cool lemmeseeit");
     if (calcInput.innerHTML.length < 9)    
@@ -17,6 +23,10 @@ function numbInput () {
         else {
             calcInput.innerHTML += this.innerHTML;
         }
+}
+
+function clearAll () {
+    calcInput.innerHTML = "0";
 }
 
 
