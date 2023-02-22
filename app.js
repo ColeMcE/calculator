9b45ba273ccfaaf3bea077d1d0a1eee401ae5fe4
ColeMcE <27023447+ColeMcE@users.noreply.github.com>
2023-02-22 13:40:05 -0600

    Up to date for pc

---
+++ app.js
@@ -0,0 +1,9 @@
+let buttons = document.getElementsByClassName("button");
+for (let index = 0; index < buttons.length; index++) {
+    buttons[i].addEventListener("click", onClick);
+    
+}
+
+function onClick () {
+    console.log(click);
+}
