860060fef23c7dba5cf2ab5f9dbd44d4a28df26d
ColeMcE <27023447+ColeMcE@users.noreply.github.com>
2023-03-03 12:22:06 -0600

    Add operation ability, bugs still present

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
-//Declare Variables
-let numberButtons = document.getElementsByClassName("number");
-let operatorButtons = document.getElementsByClassName("operator");
-let calcInput = document.getElementById("number-display");
-let allClear = document.getElementById("ac-button");
-let clear = document.getElementById("clear-button");
+//Declare buttons
+const numberButtons = document.getElementsByClassName("number");
+const operatorButtons = document.getElementsByClassName("operator");
+const calcInput = document.getElementById("number-display");
+const allClear = document.getElementById("ac-button");
+const clear = document.getElementById("clear-button");
+const equals = document.getElementById("equal");
+//Declare operation variables
+let storedNumber = '';
+let clickedOperator = ''
+let firstNumber = '';
+let result = '';
+//Logic flags
+let equalsFlag = true;
+
+
 
 //Set initial display value
-calcInput.innerHTML = "0";
+calcInput.innerText = "0";
 
 //Add event listeners
 for (let i = 0; i < numberButtons.length; i++) {
@@ -19,57 +29,102 @@ for (let i = 0; i < operatorButtons.length; i++) {
 }
 allClear.addEventListener("click", clearAll);
 clear.addEventListener("click", clearLast)
+equals.addEventListener("click", equalsClick);
 
 //Updates number display when number is clicked
 function numbInput () {
-    if (calcInput.innerHTML.length < 9)    
-        if (calcInput.innerHTML == "0") {
-            calcInput.innerHTML = this.innerHTML
+    if (equalsFlag == true) {
+        if (calcInput.innerText.length < 9)    
+        if (calcInput.innerText == "0") {
+            storedNumber = this.innerText;
+            calcInput.innerText = storedNumber;
         }
         else {
-            calcInput.innerHTML += this.innerHTML;
+            storedNumber += this.innerText;
+            calcInput.innerText = storedNumber;
         }
+    }
+    else {
+        clearAll();
+        if (calcInput.innerText.length < 9)    
+        if (calcInput.innerText == "0") {
+            storedNumber = this.innerText;
+            calcInput.innerText = storedNumber;
+        }
+        else {
+            storedNumber += this.innerText;
+            calcInput.innerText = storedNumber;
+        }
+    }
+    
+}
+
+function operationClick () {
+    if (firstNumber && storedNumber) {
+        //equals function
+        //oper[this.id](parseFloat(firstNumber), parseFloat(storedNumber));
+        displayResult();
+      }
+    firstNumber = storedNumber;
+    clickedOperator = this.id;
+    console.log(this.id);
+    calcInput.innerText = storedNumber + this.innerText;
+    storedNumber = "";
+    console.log('FirstNumber' + firstNumber + 'Stored' + storedNumber);
+
+}
+
+function displayResult () {
+    result = parseFloat(oper[clickedOperator](parseFloat(firstNumber), parseFloat(storedNumber)));
+    calcInput.innerText = result;
+    storedNumber = result;
+}
+
+function equalsClick () {
+    equalsFlag = false;
+    displayResult();
+    
 }
 
 //Sets display value back to zero
 function clearAll () {
-    calcInput.innerHTML = "0";
-    //May need to add a reset for a stored value later
+    storedNumber = "0";
+    firstNumber = "";
+    calcInput.innerText = storedNumber;
+    equalsFlag = true;
+    
+    
 }
 
 //Removes last digit entered, resetting to zero if only one left
 function clearLast () {
-    let removeOne = calcInput.innerHTML;
-    console.log(removeOne);
+    let removeOne = storedNumber;
+    console.log(storedNumber.length);
     if (removeOne.length > 1) {
         removeOne = removeOne.slice(0, -1);
-        calcInput.innerHTML = removeOne;
+        storedNumber = removeOne;
+        calcInput.innerText = storedNumber;
     }
     else if (removeOne.length == 1) {
         clearAll();
     }
 }
 
-function operationClick () {
-    oper[this.id]();
-
-}
-
 let oper = {
     add: function(x,y) {
-        (x + y);
+        return (x + y);
          console.log("add");
     },
     sub: function(x,y) {
-        (x - y);
+        return (x - y);
          console.log("subtract");
     },
     mult: function(x,y) {
-        (x * y);
+        return (x * y);
         console.log("multiply");
     },
     div: function(x,y) {
-        (x / y);
+        return (x / y);
          console.log("divide");
     }
   }
